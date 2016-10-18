ddbc74c0bc165ed0c9ca26b4b17dedf13ea9a451
Pierre Toroyan <p.toroyan@gmail.com>
2016-10-18 13:14:51 -0700

    NumberFormatter for detail info
    - created NumberFormatter() for stock info
    - .currency & .decimal

--- StockTrack/StockDetailViewController.swift
+++ StockTrack/StockDetailViewController.swift
@@ -51,25 +51,65 @@ class StockDetailViewController: UIViewController {
     func getStockInfo() {
         guard let stockSymbol = stock?.symbol else {return}
         
-		StockController.getStockInfo(stockSymbol) { (stockInfo) in
-            guard let stockInfo = stockInfo else {return}
-            
-            guard let stockPrice = stockInfo.lastPrice,
-                let openPrice = stockInfo.open,
-                let highPrice = stockInfo.high,
-                let lowPrice = stockInfo.low,
-                let stockVolume = stockInfo.volume,
-                let stockMarketCap = stockInfo.marketCap else {return}
-            
-            DispatchQueue.main.async {
-                self.stockPrice.text = "$\(stockPrice)"
-                self.openPrice.text = "$\(openPrice)"
-                self.highPrice.text = "$\(highPrice)"
-                self.lowPrice.text = "$\(lowPrice)"
-                self.stockVolume.text = "\(stockVolume)"
-                self.stockMarketCap.text = "\(stockMarketCap)"
+        DispatchQueue.global(qos: .background).async {
+            StockController.getStockInfo(stockSymbol) { (stockInfo) in
+                guard let stockInfo = stockInfo else {return}
+                
+                guard let stockPrice = stockInfo.lastPrice,
+                    let openPrice = stockInfo.open,
+                    let highPrice = stockInfo.high,
+                    let lowPrice = stockInfo.low,
+                    let stockVolume = stockInfo.volume,
+                    let stockMarketCap = stockInfo.marketCap else {return}
+                
+                let numberFormatter = NumberFormatter()
+                numberFormatter.numberStyle = .decimal
+                
+                let dollarFormatter = NumberFormatter()
+                dollarFormatter.numberStyle = .currency
+               
+                guard let convertedStockPrice = dollarFormatter.string(from: NSNumber(value: stockPrice)) else {return}
+                guard let convertedOpenPrice = dollarFormatter.string(from: NSNumber(value: openPrice)) else {return}
+                guard let convertedHighPrice = dollarFormatter.string(from: NSNumber(value: highPrice)) else {return}
+                guard let convertedLowPrice = dollarFormatter.string(from: NSNumber(value: lowPrice)) else {return}
+
+            	guard let convertedStockVolume = numberFormatter.string(from: NSNumber(value: stockVolume)) else {return}
+                guard let convertedMarketCap = numberFormatter.string(from: NSNumber(value: stockMarketCap)) else {return}
+            	
+                DispatchQueue.main.async {
+                    self.stockPrice.text = "\(convertedStockPrice)"
+                    self.openPrice.text = "\(convertedOpenPrice)"
+                    self.highPrice.text = "\(convertedHighPrice)"
+                    self.lowPrice.text = "\(convertedLowPrice)"
+                    self.stockVolume.text = "\(convertedStockVolume)"
+                    self.stockMarketCap.text = "\(convertedMarketCap)"
+                }
             }
         }
     }
 
 }
+
+
+
+//        DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+//            StockController.getStockInfo(stockSymbol) { (stockInfo) in
+//                guard let stockInfo = stockInfo else {return}
+//
+//                guard let stockPrice = stockInfo.lastPrice,
+//                    let openPrice = stockInfo.open,
+//                    let highPrice = stockInfo.high,
+//                    let lowPrice = stockInfo.low,
+//                    let stockVolume = stockInfo.volume,
+//                    let stockMarketCap = stockInfo.marketCap else {return}
+//
+//                DispatchQueue.global().async {
+//                    self.stockPrice.text = "$\(stockPrice)"
+//                    self.openPrice.text = "$\(openPrice)"
+//                    self.highPrice.text = "$\(highPrice)"
+//                    self.lowPrice.text = "$\(lowPrice)"
+//                    self.stockVolume.text = "\(stockVolume)"
+//                    self.stockMarketCap.text = "\(stockMarketCap)"
+//                }
+//        	}
+//        }
